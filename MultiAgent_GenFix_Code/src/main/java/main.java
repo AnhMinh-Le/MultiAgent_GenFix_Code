5b1d836f1b994f20fff91aabbf96de4577cd4df4
giachu19705 <163003215+giachu19705@users.noreply.github.com>
2024-12-31 01:24:30 +0700

    reconstruct_project

---
+++ MultiAgent_GenFix_Code/src/main/java/main.java
@@ -0,0 +1,22 @@
+package main.java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/resources/view/GENCODE.fxml"));
+        Scene scene = new Scene(loader.load(), 850, 588);
+        primaryStage.setTitle("Code Generator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
